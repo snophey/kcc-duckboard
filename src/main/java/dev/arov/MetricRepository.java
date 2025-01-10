2ab9ae9a1df9068d41dd502a9a44aeadc72e42c3
Alexander Rovner <rovnero@gmail.com>
2025-01-10 17:59:05 +0100

    add API for querying DB

--- src/main/java/dev/arov/MetricRepository.java
+++ src/main/java/dev/arov/MetricRepository.java
@@ -6,17 +6,26 @@ import io.quarkus.logging.Log;
 import io.quarkus.runtime.StartupEvent;
 import io.spoud.kcc.data.AggregatedDataWindowed;
 import jakarta.enterprise.event.Observes;
+import jakarta.ws.rs.BadRequestException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.duckdb.DuckDBConnection;
+import org.duckdb.DuckDBTimestamp;
 import org.eclipse.microprofile.reactive.messaging.*;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.sql.Timestamp;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 public class MetricRepository {
@@ -39,12 +48,13 @@ public class MetricRepository {
     private void createTableIfNotExists(DuckDBConnection connection) throws SQLException {
         try (var statement = connection.createStatement()) {
             statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
-                    "start_time INTEGER, " +
-                    "end_time INTEGER, " +
-                    "initial_metric_name VARCHAR, " +
-                    "tags VARCHAR, " +
-                    "context VARCHAR, " +
-                    "value DOUBLE)");
+                    "start_time TIMESTAMP_MS NOT NULL, " +
+                    "end_time TIMESTAMP_MS NOT NULL, " +
+                    "initial_metric_name VARCHAR NOT NULL, " +
+                    "name VARCHAR NOT NULL, " +
+                    "tags JSON NOT NULL, " +
+                    "context JSON NOT NULL, " +
+                    "value DOUBLE NOT NULL)");
         }
     }
 
@@ -76,6 +86,8 @@ public class MetricRepository {
                 for (var wrappedMetric : wrappedMetrics) {
                     var metric = unwrapFunction.apply(wrappedMetric);
                     Log.debugv("Ingesting metric: {0}", metric);
+                    var start = DuckDBTimestamp.fromMilliInstant(metric.getStartTime().toEpochMilli());
+                    var end = DuckDBTimestamp.fromMilliInstant(metric.getEndTime().toEpochMilli());
                     var tags = "";
                     var context = "";
                     try {
@@ -87,9 +99,10 @@ public class MetricRepository {
                         continue;
                     }
                     appender.beginRow();
-                    appender.append(metric.getStartTime().toEpochMilli());
-                    appender.append(metric.getEndTime().toEpochMilli());
+                    appender.appendLocalDateTime(start.toLocalDateTime());
+                    appender.appendLocalDateTime(end.toLocalDateTime());
                     appender.append(metric.getInitialMetricName());
+                    appender.append(metric.getName());
                     appender.append(tags);
                     appender.append(context);
                     appender.append(metric.getValue());
@@ -101,6 +114,36 @@ public class MetricRepository {
             }
             Log.infof("Ingestion complete. Skipped %d metrics.", skipped);
         });
+    }
+
+    public Set<String> getAllTagKeys() {
+        return getAllJsonKeys("tags");
+    }
+
+    public Set<String> getAllContextKeys() {
+        return getAllJsonKeys("context");
+    }
+
+    private Set<String> getAllJsonKeys(String column) {
+        return getConnection()
+                .map(conn -> {
+                    try (var statement = conn.prepareStatement("SELECT DISTINCT json_keys( ? ) FROM " + TABLE_NAME)) {
+                        statement.setString(1, column);
+                        var result = statement.executeQuery();
+                        var keys = new HashSet<String>();
+                        while (result.next()) {
+                            var key = result.getString(1);
+                            if (key != null) {
+                                keys.add(key.substring(1, key.length() - 1)); // remove brackets
+                            }
+                        }
+                        return keys;
+                    } catch (Exception e) {
+                        Log.error("Failed to get keys of column: " + column, e);
+                    }
+                    return new HashSet<String>();
+                })
+                .orElse(new HashSet<>());
     }
 
     public double getMetricAverage(String metricName) {
@@ -118,5 +161,236 @@ public class MetricRepository {
                     return 0.0;
                 })
                 .orElse(0.0);
+    }
+
+    public List<AggregatedDataWindowed> getAggregatedMetric(String metricName,
+                                                      AggregationType aggType,
+                                                      FilterSpec tagFilter,
+                                                      TimestampParam startTimestamp,
+                                                      TimestampParam endTimestamp,
+                                                      GroupBySpec groupBy,
+                                                      Integer limit,
+                                                      Integer offset,
+                                                      SortOrder sort) {
+        return getConnection()
+                .map(conn -> {
+                    try {
+                        var params = new ArrayList<>();
+                        var query = new StringBuilder("SELECT ");
+
+                        for (var tag : groupBy.tags()) {
+                            params.add(tag);
+                            params.add("tag:" + tag);
+                        }
+                        for (var ctx : groupBy.contexts()) {
+                            params.add(ctx);
+                            params.add("ctx:" + ctx);
+                        }
+
+                        var selected = Stream.concat(
+                                IntStream.range(0, groupBy.contexts().size()).mapToObj(i -> "context->>? as ?"),
+                                IntStream.range(0, groupBy.tags().size()).mapToObj(i -> "tags->>? as ?")
+                        ).collect(Collectors.joining(", ", " ", " "));
+
+                        if (groupBy.groupByResourceName()) {
+                            query.append(", name");
+                        }
+
+                        query.append(selected).append(", ");
+                        switch (aggType) {
+                            case AVG -> query.append("AVG(value) as value");
+                            case SUM -> query.append("SUM(value) as value");
+                            case COUNT -> query.append("COUNT(value) as value");
+                            case MIN -> query.append("MIN(value) as value");
+                            case MAX -> query.append("MAX(value) as value");
+                        }
+                        query.append(" FROM ").append(TABLE_NAME).append(" WHERE initial_metric_name = ?");
+                        params.add(metricName);
+                        if (startTimestamp != null) {
+                            query.append(" AND start_time >= ?");
+                            params.add(Timestamp.valueOf(startTimestamp.timestamp()));
+                        }
+                        if (endTimestamp != null) {
+                            query.append(" AND end_time <= ?");
+                            params.add(Timestamp.valueOf(endTimestamp.timestamp()));
+                        }
+                        for (var filter : tagFilter.tagFilters()) {
+                            query.append(" AND (tags->>? = ?)");
+                            params.add(filter.key());
+                            params.add(filter.value());
+                        }
+                        for (var filter : tagFilter.contextFilters()) {
+                            query.append(" AND (context->>? = ?)");
+                            params.add(filter.key());
+                            params.add(filter.value());
+                        }
+                        if (groupBy.groupByResourceName() || (!groupBy.tags().isEmpty() || !groupBy.contexts().isEmpty())) {
+                            query.append(" GROUP BY ");
+                            query.append(groupBy.tags().stream().map(k -> String.format("\"tag:%s\"", k)).collect(Collectors.joining(", ")));
+                            if (!groupBy.tags().isEmpty() && !groupBy.contexts().isEmpty())
+                                query.append(", ");
+                            query.append(groupBy.contexts().stream().map(k -> String.format("\"ctx:%s\"", k)).collect(Collectors.joining(", ")));
+                            if (groupBy.groupByResourceName()) {
+                                query.append(", name");
+                            }
+                        }
+                        if (sort != null) {
+                            query.append(" ORDER BY value ");
+                            query.append(sort);
+                        }
+                        if (limit != null) {
+                            query.append(" LIMIT ?");
+                            params.add(limit);
+                        }
+                        if (offset != null) {
+                            query.append(" OFFSET ?");
+                            params.add(offset);
+                        }
+                        try (var statement = conn.prepareStatement(query.toString())) {
+                            for (int i = 0; i < params.size(); i++) {
+                                statement.setObject(i + 1, params.get(i));
+                            }
+                            var result = statement.executeQuery();
+                            var metrics = new ArrayList<AggregatedDataWindowed>();
+                            var startTime = startTimestamp != null ? startTimestamp.timestamp().atZone(ZoneId.systemDefault()).toInstant() : Instant.EPOCH;
+                            var endTime = endTimestamp != null ? endTimestamp.timestamp().atZone(ZoneId.systemDefault()).toInstant() : Instant.now();
+                            while (result.next()) {
+                                var aggValue = result.getDouble("value");
+                                var name = groupBy.groupByResourceName() ? result.getString("name") : "unknown";
+                                var tags = new HashMap<String, String>();
+                                var context = new HashMap<String, String>();
+                                for (var tag : groupBy.tags()) {
+                                    tags.put(tag, result.getString("tag:" + tag));
+                                }
+                                for (var ctx : groupBy.contexts()) {
+                                    context.put(ctx, result.getString("ctx:" + ctx));
+                                }
+                                metrics.add(AggregatedDataWindowed.newBuilder()
+                                                .setContext(context)
+                                                .setTags(tags)
+                                                .setInitialMetricName(metricName)
+                                                .setName(name)
+                                                .setStartTime(startTime)
+                                                .setEndTime(endTime)
+                                                .setValue(aggValue)
+                                        .build());
+                            }
+                            return metrics;
+                        }
+                    } catch (SQLException e) {
+                        Log.error("Failed to get aggregated metric", e);
+                    }
+                    return null;
+                })
+                .orElse(null);
+    }
+
+
+    // only for debugging purposes
+    public String runQuery(String query) {
+        return getConnection()
+                .map(conn -> {
+                    try (var statement = conn.prepareStatement(query)) {
+                        var result = statement.executeQuery();
+                        var rows = new ArrayList<Map<String, Object>>();
+                        while (result.next()) {
+                            var row = new HashMap<String, Object>();
+                            for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                                row.put(result.getMetaData().getColumnName(i), result.getObject(i));
+                            }
+                            rows.add(row);
+                        }
+                        return OBJECT_MAPPER.writeValueAsString(rows);
+                    } catch (SQLException | JsonProcessingException e) {
+                        Log.error("Failed to run query", e);
+                    }
+                    return "";
+                })
+                .orElse("");
+    }
+
+    public record GroupBySpec(List<String> tags, List<String> contexts, boolean groupByResourceName) {
+        public static GroupBySpec fromString(String value) {
+            var parts = value.split(",");
+            var tags = new ArrayList<String>();
+            var contexts = new ArrayList<String>();
+            var groupByResourceName = false;
+            for (var part : parts) {
+                if (part.startsWith("tag:")) {
+                    var tagKey = part.substring(4);
+                    if (!tagKey.isBlank()) {
+                        tags.add(tagKey);
+                    }
+                } else if (part.startsWith("context:")) {
+                    var contextKey = part.substring(8);
+                    if (!contextKey.isBlank()) {
+                        contexts.add(contextKey);
+                    }
+                } else if (part.startsWith("builtin:")) {
+                    var builtinKey = part.substring(8);
+                    groupByResourceName = builtinKey.equals("resourceName");
+                }
+            }
+            return new GroupBySpec(tags, contexts, groupByResourceName);
+        }
+    }
+
+    public record FilterPair(String key, String value) {}
+
+    public record FilterSpec(List<FilterPair> tagFilters, List<FilterPair> contextFilters) {
+        // a filter spec is a comma separated list of the form type:key:value, where type is either "tag" or "context"
+        public static FilterSpec fromString(String value) {
+            var parts = value.split(",");
+            var tagFilters = new ArrayList<FilterPair>();
+            var contextFilters = new ArrayList<FilterPair>();
+            for (var part : parts) {
+                try {
+                    var split = part.split(":");
+                    var type = split[0];
+                    var key = split[1];
+                    var val = split[2];
+                    if (type.equals("tag")) {
+                        tagFilters.add(new FilterPair(key, val));
+                    } else if (type.equals("context")) {
+                        contextFilters.add(new FilterPair(key, val));
+                    } else {
+                        throw new BadRequestException("Invalid filter type. Expected 'tag' or 'context'");
+                    }
+                } catch (Exception e) {
+                    throw new BadRequestException("Invalid filter format. Expected comma-separated list of type:key:value triplets, e.g. tag:region:us-west-1,context:environment:prod", e);
+                }
+            }
+            return new FilterSpec(tagFilters, contextFilters);
+        }
+    }
+
+    public record TimestampParam(LocalDateTime timestamp) {
+        public static TimestampParam fromString(String value) {
+            try {
+                return new TimestampParam(LocalDateTime.parse(value, DateTimeFormatter.ISO_DATE_TIME));
+            } catch (DateTimeParseException e) {
+                throw new BadRequestException("Invalid timestamp format in start or end timestamp. Expected ISO-8601 format with an optional timezone offset, e.g. 2021-01-01T00:00:00+01:00", e);
+            }
+        }
+    }
+
+    public enum SortOrder {
+        ASC, DESC;
+
+        public static SortOrder fromString(String value) {
+            try {
+                return SortOrder.valueOf(value.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new BadRequestException("Invalid sort order. Expected 'asc' or 'desc'");
+            }
+        }
+    }
+
+    public enum AggregationType {
+        AVG, SUM, COUNT, MIN, MAX;
+
+        public static AggregationType fromString(String value) {
+            return AggregationType.valueOf(value.toUpperCase());
+        }
     }
 }
--- src/main/java/dev/arov/MetricResource.java
+++ src/main/java/dev/arov/MetricResource.java
@@ -1,23 +1,71 @@
 package dev.arov;
 
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.configuration.ConfigUtils;
 import io.smallrye.common.annotation.Blocking;
+import io.spoud.kcc.data.AggregatedDataWindowed;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.RestPath;
+import org.jboss.resteasy.reactive.RestQuery;
+
+import java.util.List;
+import java.util.Set;
 
 @Produces(MediaType.APPLICATION_JSON)
-@Path("/metrics")
+@Path("/api/metrics")
+@Blocking
 public class MetricResource {
     @Inject
     MetricRepository metricRepository;
 
-    @Blocking //  we are talking to the database, which could take a while
     @GET
-    @Path("/average/{metricName}")
-    public double getMetric(@RestPath String metricName) {
-        return metricRepository.getMetricAverage(metricName);
+    @Path("/{metricName}/{aggType}")
+    public List<AggregatedDataWindowed> getAggregatedMetric(@RestPath String metricName,
+                                                            @RestPath MetricRepository.AggregationType aggType,
+                                                            @RestQuery MetricRepository.FilterSpec filter,
+                                                            @RestQuery MetricRepository.TimestampParam startTimestamp,
+                                                            @RestQuery MetricRepository.TimestampParam endTimestamp,
+                                                            @RestQuery MetricRepository.GroupBySpec groupBy,
+                                                            @RestQuery Integer limit,
+                                                            @RestQuery Integer offset,
+                                                            @RestQuery MetricRepository.SortOrder sort) {
+        Log.infof("All params: metric=%s, aggType=%s, filter=%s, start=%s, end=%s, groupBy=%s, limit=%s, offset=%s, sort=%s",
+                metricName, aggType, filter, startTimestamp, endTimestamp, groupBy, limit, offset, sort);
+        return metricRepository.getAggregatedMetric(metricName,
+                aggType,
+                filter == null ? new MetricRepository.FilterSpec(List.of(), List.of()) : filter,
+                startTimestamp,
+                endTimestamp,
+                groupBy == null ? new MetricRepository.GroupBySpec(List.of(), List.of(), false) : groupBy,
+                limit != null && limit < 0 ? null : limit,
+                offset != null && offset < 0 ? null : offset,
+                sort);
+    }
+
+    @GET
+    @Path("/tags")
+    public Set<String> getTags() {
+        return metricRepository.getAllTagKeys();
+    }
+
+    @GET
+    @Path("/contexts")
+    public Set<String> getContexts() {
+        return metricRepository.getAllContextKeys();
+    }
+
+    // only for testing purposes (disable in production)
+    @POST
+    @Path("/query")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String runQuery(String query) {
+        if (!ConfigUtils.getProfiles().contains("test") && !ConfigUtils.getProfiles().contains("dev")) {
+            Log.warn("Someone tried to run the following query in production: " + query);
+            Log.warn("If this was you, remember that this endpoint is only available if QUARKUS_PROFILE=test or dev. Current profiles: " + ConfigUtils.getProfiles());
+            throw new NotFoundException();
+        }
+        return metricRepository.runQuery(query);
     }
 }
